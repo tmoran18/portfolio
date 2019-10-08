3a4678a2baf5d3e2e1c3e5e18cd32e75eaeeaabb
tmoran18 <moran.tim01@gmail.com>
2019-10-08 20:04:16 +1000

    got swup functionality working added wrapper function

--- index.js
+++ index.js
@@ -1,42 +1,61 @@
-// Mobile Menu Toggle
-var menu = document.querySelector('.menu-container');
-var hamburger = document.getElementById('toggle');
-var toggleStatus = 0;
-
 const swup = new Swup();
+init();
+swup.on('contentReplaced', init);
 
-// If the window is below Media query size
-//   uncheck the hamburger Input
-//   close the mobile navigation
-function checkWindowSize(x) {
-  if (!x.matches) { // If media query matches
-    hamburger.checked = false;
-    closeNav();
+
+function init() {
+  if (document.querySelector('#toggle')) {
+    new mobileNav();
   }
-}
+};
 
-var windowSize = window.matchMedia("(max-width: 900px)")
-checkWindowSize(windowSize) // Call listener function at run time
-windowSize.addListener(checkWindowSize) // Attach listener function on state changes
+var mobileNavStart = new mobileNav();
+
+function mobileNav() { // Wrapper function
+
+  var menu = document.querySelector('.menu-container');
+  var hamburger = document.getElementById('toggle');
+  var toggleStatus = 0;
+
+  // If the window is below Media query size 
+  //  uncheck the hamburger Input close the mobile navigation
+  function checkWindowSize(x) {
+    if (!x.matches) { // If media query matches
+      hamburger.checked = false;
+      closeNav();
+    }
+  };
+
+  // Opens the DropDown mobile Navigation
+  function openNav() {
+    menu.style.top = ("90px");
+    toggleStatus = 1;
+    console.log("opening");
+  };
+
+  // Closes the DropDown mobile Navigation
+  function closeNav() {
+    menu.style.top = ("-350px");
+    toggleStatus = 0;
+    console.log("closing");
+  };
+
+  // Toggles the opening and closing of the mobile navigation
+  function toggleMenu() {
+    if (toggleStatus === 0) {
+      openNav();
+    } else if (toggleStatus === 1) {
+      closeNav();
+    }
+  };
 
 
-// Opens the DropDown mobile Navigation
-function openNav() {
-  menu.style.top = ("90px");
-  toggleStatus = 1;
-}
-// Closes the DropDown mobile Navigation
-function closeNav() {
-  menu.style.top = ("-350px");
-  toggleStatus = 0;
-}
-// Toggles the opening and closing of the mobile navigation
-function toggleMenu() {
-  if (toggleStatus === 0) {
-    openNav();
-  } else if (toggleStatus === 1) {
-    closeNav();
-  }
-}
-// Event listener on the Hamburger menu to open and close the navigation
-hamburger.addEventListener('click', toggleMenu);
+  var windowSize = window.matchMedia("(max-width: 900px)")
+  checkWindowSize(windowSize) // Call listener function at run time
+  windowSize.addListener(checkWindowSize) // Attach listener function on state changes
+
+  // Event listener on the Hamburger menu to open and close the navigation
+  hamburger.addEventListener('click', toggleMenu);
+
+};
+
