64a04394940722d73e8733d3ad362fde3cb11ea2
tmoran18 <moran.tim01@gmail.com>
2019-10-21 21:56:05 +1000

    working on contact form

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+// Instatian Swup and run init function
 const swup = new Swup();
 init();
 swup.on('contentReplaced', init);
 
-
+// Check if the toggle id is on this page then run the init function.
 function init() {
   if (document.querySelector('#toggle')) {
     new mobileNav();
@@ -12,11 +13,9 @@ function init() {
 var mobileNavStart = new mobileNav();
 
 function mobileNav() { // Wrapper function
-
   var menu = document.querySelector('.menu-container');
   var hamburger = document.getElementById('toggle');
   var toggleStatus = 0;
-
   // If the window is below Media query size 
   //  uncheck the hamburger Input close the mobile navigation
   function checkWindowSize(x) {
@@ -25,21 +24,20 @@ function mobileNav() { // Wrapper function
       closeNav();
     }
   };
-
   // Opens the DropDown mobile Navigation
   function openNav() {
     menu.style.top = ("90px");
     toggleStatus = 1;
+    document.querySelector('.form-container').style.transform = "translateY(17rem)";
     console.log("opening");
   };
-
   // Closes the DropDown mobile Navigation
   function closeNav() {
     menu.style.top = ("-350px");
     toggleStatus = 0;
+    document.querySelector('.form-container').style.transform = "translateY(0)";
     console.log("closing");
   };
-
   // Toggles the opening and closing of the mobile navigation
   function toggleMenu() {
     if (toggleStatus === 0) {
@@ -48,14 +46,12 @@ function mobileNav() { // Wrapper function
       closeNav();
     }
   };
-
-
   var windowSize = window.matchMedia("(max-width: 900px)")
   checkWindowSize(windowSize) // Call listener function at run time
   windowSize.addListener(checkWindowSize) // Attach listener function on state changes
-
   // Event listener on the Hamburger menu to open and close the navigation
   hamburger.addEventListener('click', toggleMenu);
+
 
 };
 
